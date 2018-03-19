4bfa937b8dc85db10fb87e92f775ebb3bb5f9a8d
Diffela <amberdusk@163.com>
2018-03-20 00:36:40 +0800

    ref 001

---
+++ learn_python_the_hard_way_coding/ex01.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print('Yay!Printing')
+print("I'd much rather you 'not'")
+print('I "said" do not touch this.')
+
+
+#Drills
+print('I "said" do not touch this.')
+
+print('I "said" do not touch this.')
+
+#print('I "said" do not touch this.')
---
+++ learn_python_the_hard_way_coding/ex02.py
@@ -0,0 +1,8 @@
+
+# this chapter has nothing to do
+
+#print("this won't run")
+
+print('this will run')
+
+#Drills
